7156172cdbd50e87239af29dca9a488c7a72be57
Anastasiia <milonova.nastasia@gmail.com>
2020-10-15 22:47:37 +0300

    finish

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const body = document.querySelector('body');
+const card = document.querySelector('.flipper');
+const back = document.querySelector('.back');
+const front = document.querySelector('.front');
+const video = document.querySelector('#myVideo');
 
 
 burgerMenu.addEventListener('click',function(){
   this.classList.toggle("close");
-  body.classList.toggle("active");
+});
+
+card.addEventListener('click',function(){
+  this.classList.toggle("flipped");
+});
+
+front.addEventListener('click',function(){
+  video.play();
+});
+
+back.addEventListener('mouseover',function(){
+  video.play();
+});
+
+back.addEventListener('mouseout',function(){
+  video.pause();
+});
+
+video.addEventListener('click',function(){
+  video.pause();
 });
